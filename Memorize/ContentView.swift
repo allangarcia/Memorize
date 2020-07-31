10cf949f19afe683b60ef6dc9158fecb315f5993
Allan Garcia <allan.garcia@gmail.com>
2020-07-31 22:24:18 +0100

    Lecture 1 commit

---
+++ Memorize/ContentView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Allan Garcia on 31/07/2020.
+//  Copyright © 2020 Allan Garcia. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        HStack {
+            ForEach(0..<4) { index in
+                CardView(isFaceUp: true)
+            }
+        }
+        .padding()
+        .foregroundColor(Color.orange)
+    }
+}
+
+struct CardView: View {
+    var isFaceUp: Bool
+    var body: some View {
+        ZStack {
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
+                RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3)
+                Text("👻").font(Font.largeTitle)
+            } else {
+                RoundedRectangle(cornerRadius: 10.0).fill(Color.orange)
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
