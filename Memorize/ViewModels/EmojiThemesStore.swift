e41b67596d9cf1afd25e495512ec232da2ec55c7
Allan Garcia <allan.garcia@gmail.com>
2020-11-26 15:58:34 -0300

    Starting the solution of Assignment 6

---
+++ Memorize/ViewModels/EmojiThemesStore.swift
@@ -0,0 +1,15 @@
+//
+//  EmojiThemesStore.swift
+//  Memorize
+//
+//  Created by Allan Garcia on 26/11/2020.
+//  Copyright © 2020 Allan Garcia. All rights reserved.
+//
+
+import SwiftUI
+
+class EmojiThemesStore: ObservableObject {
+    
+    var themes: Array<EmojiMemoryGame.Theme>?
+
+}
