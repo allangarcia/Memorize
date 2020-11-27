5e5d49aa5b3ef09faa9a854b5b11aa79648a3c61
Allan Garcia <allan.garcia@gmail.com>
2020-11-26 21:20:15 -0300

    Implementing the UserDefaults persistence (req. task #11)

--- Memorize/ViewModels/EmojiThemesStore.swift
+++ Memorize/ViewModels/EmojiThemesStore.swift
@@ -7,13 +7,53 @@
 //
 
 import SwiftUI
+import Combine
 
 class EmojiThemesStore: ObservableObject {
     
-    var themes: Array<EmojiMemoryGame.Theme>
+    let name: String
+
+    @Published var themes = Array<EmojiMemoryGame.Theme>()
     
-    init() {
-        self.themes = EmojiMemoryGame.themes
+    private var autosave: AnyCancellable?
+    
+    init(named name: String = "Themes") {
+        let defaultsKey = "Memorize.\(name)"
+
+        self.name = name
+        
+        if let blob = UserDefaults.standard.object(forKey: defaultsKey) {
+            self.themes = Array(fromPropertyList: blob)
+        } else {
+            self.themes = EmojiMemoryGame.themes
+        }
+
+        autosave = $themes.sink { themes in
+            UserDefaults.standard.set(themes.asPropertyList, forKey: defaultsKey)
+        }
     }
 
+
+
 }
+
+extension Array where Element == EmojiMemoryGame.Theme {
+    
+    var asPropertyList: [Data] {
+        self.map { element -> Data in
+            element.json! // crash if can't get a json representation from theme struct
+        }
+    }
+    
+    init(fromPropertyList plist: Any?) {
+        self.init()
+        if let dataArray = plist as? [Data] {
+            for data in dataArray {
+                if let theme = EmojiMemoryGame.Theme(json: data) {
+                    self.append(theme)
+                }
+            }
+        }
+    }
+}
+
